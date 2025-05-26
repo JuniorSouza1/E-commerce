bc6bce1099b3089075169911a2cb7fc602ee8543
Junior Souza <164554082+JuniorSouza1@users.noreply.github.com>
2025-05-26 08:24:21 -0300

    Up

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
           <div className="flex justify-between items-center py-4">
             {/* Logo e ícone do carrinho */}
             <div className="flex justify-start items-center">
-              <span className="text-xl sm:text-2xl font-italic text-[#FF4500] rounded-md px-2 sm:px-4 py-2 scale-105 ">TechStore</span>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FF4500" className="w-6 h-6 sm:w-8 sm:h-8 -ml-1 ">
+              <span className="text-xl sm:text-2xl font-italic text-[#ffff] rounded-md px-2 sm:px-4 py-2 scale-105 ">TechStore</span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ffff" className="w-6 h-6 sm:w-8 sm:h-8 -ml-1 ">
                 <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
               </svg>
             </div>
